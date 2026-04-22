f35d89dd1f23c3113ad9c4ebdcf0a9947bc55411
rmalouache-cloud <rmalouache@gmail.com>
2026-04-22 15:49:13 +0100

    Update code.py

--- code.py
+++ code.py
@@ -32,7 +32,7 @@ with col_logo:
     
     if os.path.exists(logo_path):
         logo = Image.open(logo_path)
-        st.image(logo, width=60)
+        st.image(logo, width=100)
     else:
         # Si le fichier n'existe pas, afficher un émoji par défaut
         st.markdown("# 📺")
